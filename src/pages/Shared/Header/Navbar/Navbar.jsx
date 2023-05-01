b01f2bea0e72ec5b11d1cb8a770537e89932e49d
islammazharul <islammazharul3645@gmail.com>
2023-05-02 02:01:38 +0600

    create navbar

---
+++ src/pages/Shared/Header/Navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { FaUserCircle } from 'react-icons/fa';
+import { AuthContext } from '../../../providers/AuthProvider';
+import React, { useContext } from 'react';
+
+const Navbar = () => {
+    const { user, logOut } = useContext(AuthContext)
+
+    const handleLogOut = () => {
+        logOut()
+            .then()
+            .catch(error => {
+                console.log(error)
+            })
+    }
+    return (
+        <div>
+            <Container>
+                <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mx-auto">
+                            <Link to='/category/0'>Home</Link>
+                            <Nav.Link href="#pricing">About</Nav.Link>
+                            <Nav.Link href="#pricing">Career</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            {user && <FaUserCircle style={{ fontSize: "40px" }}></FaUserCircle>}
+
+                            {user ?
+                                <Button onClick={handleLogOut} variant="outline-secondary">Log Out</Button> :
+                                <Link to="/login">
+                                    <Button variant="outline-secondary">Login</Button>
+                                </Link>
+                            }
+
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </Container>
+        </div>
+    );
+};
+
+export default Navbar;
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../providers/AuthProvider';
+import { Navigate, useLocation } from 'react-router-dom';
+import { Spinner } from 'react-bootstrap';
 
 const PrivateRoute = () => {
-    return (
-        <div>
+    const { user, loading } = useContext(AuthContext)
+    const location = useLocation();
+    console.log('user in private route', user)
 
-        </div>
-    );
+    if (loading) {
+        return <Spinner animation="border" variant="success" />
+    }
+
+    if (user) {
+        return children;
+    }
+    return <Navigate state={{ from: location }} to="/login" replace></Navigate>;
 };
 
 export default PrivateRoute;
